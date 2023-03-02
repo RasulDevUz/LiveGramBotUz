464be04dc4817c87f2c791bf4170e08c548f782b
RasulDevUz <126681184+RasulDevUz@users.noreply.github.com>
2023-03-02 03:22:08 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+blocked = "bot foydalanuvchi tomonidan bloklangan"#message that would be displayed if user has blocked you
+start = "Assalomu alaykum👋🏻 Bu bot orqali Admin bilan gaplasha olasiz✅ Admin iloji boricha tezroq javob berishga harakat qiladi👌🏻"
+ban = "admin tomonidan taqiqlangan!"#ban message
+unban = "admin tomonidan bloklangansiz."#unban message
+text_message = "Habarizgiz yuborildi✅"#answering_text to user
+banned = "siz bloklangansiz"#if admin has blocked a user
+main_id = -1001833070083 #id without quotes #can be group id
+TOKEN = "6296935998:AAFwCdwG_A1ZROBTzJI2sz8Bl_XoouIqgLY"#bot token
